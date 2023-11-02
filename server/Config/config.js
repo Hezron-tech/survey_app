b9bfc76c3738f1929c10a2c8f4138f9175a933fd
Hezron-tech <kiprophez@gmail.com>
2023-11-02 12:14:42 +0300

    connecting to mssql db

---
+++ server/Config/config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+ export const sqlConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: false, // for azure
+    trustServerCertificate: false // change to true for local dev / self-signed certs
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const express = require('express');
+
+import  express  from "express";
+import mssql from "mssql"
+import { sqlConfig } from "./Config/config.js";
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
+
+
+app.get('/', function(req,res){
+  let connection= mssql.connect(sqlConfig,(err)=>{
+    if(err){
+      console.log(err)
+    }else{
+      res.send("Db connected")
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
